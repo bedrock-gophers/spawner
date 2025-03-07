6474f8be6535a0667417c71349785a472b6a5adc
cqdetdev <101936396+cqdetdev@users.noreply.github.com>
2025-03-07 16:11:19 -0500

    feat(manual): upgrade to latest dragonfly

--- spawner/register.go
+++ spawner/register.go
@@ -17,20 +17,26 @@ func RegisterEntityType(kind world.EntityType, newEnt func(cube.Pos, *world.Tx) 
 	entities[kind.EncodeEntity()] = kind
 
 	world.RegisterItem(SpawnEgg{Kind: kind})
-	creative.RegisterItem(item.NewStack(SpawnEgg{Kind: kind}, 1))
+	creative.RegisterItem(creative.Item{
+		Stack: item.NewStack(SpawnEgg{Kind: kind}, 1),
+		Group: "Spawner",
+	})
 }
 
 func init() {
 	world.RegisterBlock(Spawner{})
 	world.RegisterItem(Spawner{})
-	creative.RegisterItem(item.NewStack(Spawner{
-		MaxNearbyEntities:   6,
-		MaxSpawnDelay:       800,
-		MinSpawnDelay:       200,
-		RequiredPlayerRange: 16,
-		Movable:             true,
-		Delay:               20,
-		SpawnCount:          4,
-		SpawnRange:          4,
-	}, 1))
+	creative.RegisterItem(creative.Item{
+		Stack: item.NewStack(Spawner{
+			MaxNearbyEntities:   6,
+			MaxSpawnDelay:       800,
+			MinSpawnDelay:       200,
+			RequiredPlayerRange: 16,
+			Movable:             true,
+			Delay:               20,
+			SpawnCount:          4,
+			SpawnRange:          4,
+		}, 1),
+		Group: "Spawner",
+	})
 }
